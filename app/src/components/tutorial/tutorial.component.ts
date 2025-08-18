e87e5a755e5a64f5db437efb022e3d65a615d296
Tommaso Pomante <gun88@hotmail.it>
2025-08-18 14:06:38 +0200

    feat: add simple tutorial overlay

---
+++ app/src/components/tutorial/tutorial.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { NgIf, NgStyle } from '@angular/common';
+import { TutorialService, TutorialState } from '../../service/tutorial.service';
+
+@Component({
+    selector: 'res-pong-user-tutorial',
+    standalone: true,
+    imports: [NgIf, NgStyle],
+    templateUrl: './tutorial.component.html',
+    styleUrl: './tutorial.component.scss'
+})
+export class TutorialComponent implements OnInit, OnDestroy {
+    private tutorial = inject(TutorialService);
+    state: TutorialState | null = null;
+    popupStyle: any = {};
+    private sub: any;
+
+    ngOnInit(): void {
+        this.sub = this.tutorial.tutorial$.subscribe(state => {
+            this.removeHighlight();
+            this.state = state;
+            if (state) {
+                const el = document.querySelector(state.step.selector) as HTMLElement | null;
+                if (el) {
+                    el.classList.add('tutorial-highlight');
+                    const rect = el.getBoundingClientRect();
+                    this.popupStyle = {
+                        top: rect.bottom + 8 + 'px',
+                        left: rect.left + 'px'
+                    };
+                    el.scrollIntoView({ behavior: 'smooth', block: 'center' });
+                } else {
+                    this.popupStyle = { top: '20px', left: '20px' };
+                }
+            }
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.sub?.unsubscribe();
+        this.removeHighlight();
+    }
+
+    next(): void {
+        this.tutorial.next();
+    }
+
+    end(): void {
+        this.tutorial.end();
+    }
+
+    private removeHighlight(): void {
+        if (this.state) {
+            const el = document.querySelector(this.state.step.selector) as HTMLElement | null;
+            el?.classList.remove('tutorial-highlight');
+        }
+    }
+}
+
