74fb9d44531d8093b7e26bd046d86e662ea63d33
tpomante <gun88@hotmail.it>
2025-08-20 21:29:32 +0200

    release 0.2.4

--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   server: '/index.php',
-  version: '0.2.3',
-  build: '20250819100028',
+  version: '0.2.4',
+  build: '20250820212851',
   hotjarSiteId: 6498948,
   hotjarVersion: 6
 }
