1e3d49ff5433f3aa5fa0c5eb7c018432a9f1c4c8
tpomante <gun88@hotmail.it>
2025-08-21 15:58:44 +0200

    release 0.2.7

--- app/src/environments/environment.dev.ts
+++ app/src/environments/environment.dev.ts
@@ -2,7 +2,5 @@ export const environment = {
   production: false,
   server: 'http://localhost:8080',
   version: 'D.E.V',
-  build: '00000000000000',
-  hotjarSiteId: 6498983,
-  hotjarVersion: 6
+  build: '00000000000000'
 }
--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -2,7 +2,5 @@ export const environment = {
   production: false,
   server: '',
   version: '0.2.7',
-  build: '20250821144104',
-  hotjarSiteId: 6498983,
-  hotjarVersion: 6
+  build: '20250821144104'
 }
