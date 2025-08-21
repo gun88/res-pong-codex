ccf4658bc883b3832701658316ab31c473b5cf19
tpomante <gun88@hotmail.it>
2025-08-21 14:42:06 +0200

    release 0.2.7

--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   server: '',
-  version: '0.2.6',
-  build: '20250820214820',
+  version: '0.2.7',
+  build: '20250821144104',
   hotjarSiteId: 6498983,
   hotjarVersion: 6
 }
